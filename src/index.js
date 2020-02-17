473c1db32e46d2f7f932eec966e238daa062f31b
Stolarskis <csstolarski@gmail.com>
2020-02-16 22:05:40 -0500

    ui scaling now has a maximum which is the current gameworld size

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import mainGame from "./mainGame.js";
 import mainMenu from "./mainMenu.js";
 
 const config = {
-  //type: Phaser.AUTO,
-  //parent: "phaser-example",
-  //width: window.innerWidth * window.devicePixelRatio,
-  //height: window.innerHeight * window.devicePixelRatio,
-
   type: Phaser.CANVAS,
-  width: window.innerWidth,
-  height: window.innerHeight,
+  width: window.innerWidth > 1920 ? 1920 : window.innerWidth,
+  height: window.innerHeight > 1080 ? 1080 : window.innerHeight,
   pixelArt: true,
   scene: [mainMenu, mainGame],
   physics: {
