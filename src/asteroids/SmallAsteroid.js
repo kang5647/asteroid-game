4ee12537166dc9bdf008001dd76dd2c9126dc429
Stolarskis <csstolarski@gmail.com>
2020-02-16 21:13:01 -0500

    Fixed small asteroids not bouncing off edges

--- src/asteroids/SmallAsteroid.js
+++ src/asteroids/SmallAsteroid.js
@@ -2,17 +2,7 @@ import Asteroid from "./Asteroid.js";
 class SmallAsteroid extends Asteroid {
   constructor(scene, x, y, rotation, speed) {
     //Use these to pass these back to the super class to construct the object
-    super(scene, x, y, "small_asteroid");
-    //Then add to scene
-    scene.add.existing(this);
-    //Add to physics group
-    scene.asteroids.add(this);
-    scene.physics.world.enableBody(this);
-    this.setCollideWorldBounds(true);
-
-    //Configuration for large asteroid
-    this.setScale(2);
-    scene.physics.velocityFromRotation(rotation, speed, this.body.velocity);
+    super(scene, x, y, "small_asteroid", rotation, speed);
   }
 
   //Upon destruction, the asteroid deletes itself.
