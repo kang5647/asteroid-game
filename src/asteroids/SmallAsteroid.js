37b3fc1dd88ce9243f27e9d04ff76156ced4d37c
Stolarskis <csstolarski@gmail.com>
2020-02-19 22:10:42 -0500

    updated asteroid hitboxes

--- src/asteroids/SmallAsteroid.js
+++ src/asteroids/SmallAsteroid.js
@@ -3,6 +3,9 @@ class SmallAsteroid extends Asteroid {
   constructor(scene, x, y, rotation, speed) {
     //Use these to pass these back to the super class to construct the object
     super(scene, x, y, "small_asteroid", rotation, speed);
+
+    this.setSize(12, 12);
+    this.setOffset(5, 5);
   }
 
   //Upon destruction, the asteroid deletes itself.
