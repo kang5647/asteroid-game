774b06902e6f72ebd6f0fd3698b24754ae3b72fa
stolarskis <csstolarski@gmail.com>
2020-07-11 22:36:53 -0400

    init commit

---
+++ terraform/modules/application/output.tf
@@ -0,0 +1,11 @@
+output "ec2_public_ip"{
+    value = aws_instance.asteroid_game.public_ip
+}
+
+output "ec2_id" {
+    value = aws_instance.asteroid_game.id
+}
+
+output "ec2_arn" {
+    value = aws_instance.asteroid_game.arn
+}
---
+++ terraform/modules/application/variables.tf
@@ -0,0 +1,38 @@
+variable "server_port"{
+    default = 80
+    description = "All internal traffic operates under http. ELB handles all https traffic externally"
+}
+
+# AMI Setup
+# Using a instance I created that has docker swarm already installed and ready to go. 
+# In future I'll setup ansible and use a generic aws AMI
+
+variable "ami_id"{
+    type = string
+}
+
+variable "instance_type"{
+    type = string
+    description = "aws instance type (ie t2-micro)"
+}
+
+variable "subnet_id"{
+    type = string
+    description = "Subnet to launch ec2 instance in. Created in network module"
+}
+
+variable "application_name"{
+    type = string
+}
+
+variable "vpc_id"{
+    type = string
+}
+
+variable "key_pair"{
+    type = string
+}
+
+variable "user_ip"{
+    type = string
+}
